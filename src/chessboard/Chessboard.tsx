b32bda37a1ee678220018c3d66b024e01857d4a9
Raymond Shiner <raymondshiner@gmail.com>
2022-11-30 13:30:49 -0800

    render full chessboard with correct css

--- src/chessboard/Chessboard.tsx
+++ src/chessboard/Chessboard.tsx
@@ -1,12 +1,44 @@
 import { FC } from "react"
+import styled from "styled-components"
 
 export const Chessboard: FC = () => {
-  const squares = [...Array(64)].map((_, index) => {
-    const squareIsWhite = index % 2 === 0
-    const squareColor = squareIsWhite ? "white" : "black"
+  const squareGrid = [...Array(8)].map((_, rowIndex) => {
+    const rowNumber = rowIndex + 1
+    const isEvenRow = rowNumber % 2 === 0
 
-    return <div data-testid={`square-${squareColor}`}>{squareColor} square</div>
+    const rowOfSquares = [...Array(8)].map((_, columnIndex) => {
+      const columnNumber = columnIndex + 1
+      const isEvenColumn = columnNumber % 2 === 0
+      const squareIsBlack = onlyOneIsTrue(isEvenRow, isEvenColumn)
+
+      const squareColor = squareIsBlack ? "black" : "white"
+      return <ChessSquare color={squareColor} />
+    })
+
+    return rowOfSquares
   })
 
-  return <div>{squares}</div>
+  return <ChessBoardWrapper>{squareGrid}</ChessBoardWrapper>
 }
+
+const onlyOneIsTrue = (a: boolean, b: boolean): boolean => {
+  return (a && !b) || (!a && b)
+}
+
+const ChessBoardWrapper = styled.div`
+  display: grid;
+  width: fit-content;
+  grid-template-columns: repeat(8, 50px);
+  grid-template-rows: repeat(8, 50px);
+  border: 3px solid blue;
+`
+
+const ChessSquare = ({ color }: { color: "white" | "black" }) => (
+  <Square data-testId={`square-${color}`} color={color} />
+)
+
+const Square = styled.div`
+  width: 50px;
+  height: 50px;
+  background: ${(props) => props.color};
+`
